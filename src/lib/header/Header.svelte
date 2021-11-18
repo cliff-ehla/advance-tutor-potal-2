dc0381ca43b7ab096aa29a354836145420f5c40a
cliff <cliff.wong@ehlacademy.org>
2021-11-18 18:58:49 +0800

    fix build error

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -19,7 +19,7 @@
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 			<li class:active={$page.path === '/login'}><a href="/login">Login</a></li>
 			<li class:active={$page.path === '/logout'}><a href="/logout">Logout</a></li>
-			<li class:active={$page.path === '/profile'}><a href="/profile">Profile</a></li>
+<!--			<li class:active={$page.path === '/profile'}><a href="/profile">Profile</a></li>-->
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
