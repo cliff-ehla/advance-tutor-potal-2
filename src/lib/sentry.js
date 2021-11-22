4084991417372190fe2318992fedbed63a67dd7a
cliff <cliff.wong@ehlacademy.org>
2021-11-22 17:49:28 +0800

    rm console log

--- src/lib/sentry.js
+++ src/lib/sentry.js
@@ -5,8 +5,7 @@ let env = import.meta.env.VITE_ENV
 
 const sentry = (() => {
 	const init = () => {
-		if (['production', 'staging', 'dev'].includes(env)) {
-			console.log('sentry init')
+		if (['production', 'staging'].includes(env)) {
 			Sentry.init({
 				dsn: sentry_dsn,
 				environment: env,
@@ -17,9 +16,8 @@ const sentry = (() => {
 	}
 
 	const log = (error) => {
-		if (['production', 'staging', 'dev'].includes(env)) {
+		if (['production', 'staging'].includes(env)) {
 			if (typeof error !== "string") error = JSON.stringify(error)
-			console.log('sentry log: ' + error)
 			Sentry.captureException(new Error(error))
 		}
 	}
