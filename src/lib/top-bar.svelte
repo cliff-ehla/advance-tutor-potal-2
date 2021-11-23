8da004a90c00df9c6b0101c2c21e880c7c82a039
cliff <cliff.wong@ehlacademy.org>
2021-11-23 11:08:02 +0800

    show profile pic

--- src/lib/top-bar.svelte
+++ src/lib/top-bar.svelte
@@ -7,9 +7,12 @@
 		<img src="/logo.png" alt="ehla" class="w-12 rounded"/>
 		<p class="ml-4 font-bold">EHLA Zoom class</p>
 	</a>
-	<div class="ml-auto hover:text-blue-500">
+	<div class="ml-auto hover:text-blue-500 flex items-center">
 		{#if user_info}
-			<a href="/profile" class="text-sm">{user_info.nickname}</a>
+			<a href="/profile" class="inline-flex items-center">
+				<img class="rounded-full w-8 h-8" src={user_info.profile_pic} alt="profile">
+				<span class="ml-2">{user_info.nickname}</span>
+			</a>
 		{:else}
 			<a href="/login">Login</a>
 		{/if}
