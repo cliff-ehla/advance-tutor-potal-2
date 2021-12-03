b770e3ad3545d2fdc19855162ec7ba504658cbf6
cliff <cliff.wong@ehlacademy.org>
2021-12-03 15:07:56 +0800

    ui

--- src/lib/zoom/zoom-preview.svelte
+++ src/lib/zoom/zoom-preview.svelte
@@ -2,6 +2,7 @@
 	import dayjs from "dayjs";
 	import Icon from '$lib/ui/icon.svelte'
 	import {getContext} from "svelte";
+	import tippy from "tippy.js";
 	export let zoom
 	const {open} = getContext('simple-modal')
 	import PdfReaderDialog from '../../components/item/item-pdf-reader/pdf-reader-dialog.svelte'
@@ -15,6 +16,12 @@
 			item_id: d.item_id
 		})
 	}
+
+	const onTippy = node => {
+		tippy(node, {
+			content: 'Open zoom application'
+		})
+	}
 </script>
 
 <div class="p-2 flex w-full">
@@ -24,9 +31,11 @@
 	<div class="w-full">
 		{#each zoom.days as d}
 			<div on:click={() => {previewMaterial(d)}} class="cursor-pointer hover:text-blue-700 hover:bg-gray-200 flex items-center mb-1 group px-1 py-0.5 rounded">
-				<Icon name="pdf" className="inline-block w-5 mr-2 text-gray-400"/>
-				<p>{d.title}</p>
-				<div class="bg-white px-2 rounded-full text-xs ml-auto hidden group-hover:block">Preview</div>
+				<Icon name="pdf" className="inline-block w-5 mr-2 text-gray-400 flex-shrink-0"/>
+				<p class="leading-tight">{d.title}</p>
+				<div class="w-14 flex-shrink-0 ml-auto">
+					<div class="bg-white px-2 rounded-full text-xs ml-auto hidden group-hover:block">Preview</div>
+				</div>
 				<!--{d.t_difficulty_rate}-->
 			</div>
 		{/each}
@@ -46,7 +55,7 @@
 			{:else}
 			{/if}
 			{#each zoom.students as s}
-				<a href="/students/{s.user_id}" class="inline-flex items-center mr-2 bg-blue-200 rounded-full mt-1 overflow-hidden">
+				<a href="/students/{s.user_id}" class="inline-flex items-center mr-2 bg-blue-200 rounded-full mt-1 overflow-hidden border border-white hover:border-blue-300">
 					<div class="w-6 h-6 rounded-full mr-1 cc text-xs bg-blue-500 text-white">{s.level.charAt(0).toUpperCase() + s.level.slice(1)}</div>
 					<span class="text-sm py-1">{s.nickname}</span>
 					<div class="w-8 cc bg-white h-8 bg-opacity-50 ml-2">
@@ -59,6 +68,13 @@
 </div>
 {#if is_today}
 	<div class="ml-24 px-2 pb-2">
-		<a href="/zoom/{zoom.wrapper_id}" class="block bg-green-500 text-white w-full py-2 rounded-full text-center">Join the class now</a>
+		<div class="flex items-center">
+			<a href="/zoom/{zoom.wrapper_id}" class="bg-gradient-to-b from-green-500 to-green-700 hover:shadow transition-transform transform hover:-translate-y-0.5 text-white py-1.5 rounded-full flex-1 flex items-center justify-center">
+				Start class
+			</a>
+			<a use:onTippy href={zoom.zoom_link} class="bg-white border-2 border-blue-500 text-blue-500 rounded-full cc ml-2 w-12 h-12">
+				<Icon name="zoom" className="w-8"/>
+			</a>
+		</div>
 	</div>
 {/if}
