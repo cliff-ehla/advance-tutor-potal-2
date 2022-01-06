b5dbc51cc290d95a6e5970d0c1eaa63ef684561d
cliff <cliff.wong@ehlacademy.org>
2022-01-06 16:07:15 +0800

    course timeline

--- src/lib/zoom/course-preview-popup.svelte
+++ src/lib/zoom/course-preview-popup.svelte
@@ -2,8 +2,7 @@
 	import {course_store} from "$lib/store/course.js";
 	import {onMount} from "svelte";
 	import dayjs from "dayjs";
-	import RateLabel from '$lib/zoom/rate-label.svelte'
-	export let zoom_list
+	import CourseTimeLine from '$lib/zoom/course-timeline.svelte'
 	export let tutor_group_id
 	$: zoom_list = $course_store[tutor_group_id]
 
@@ -26,38 +25,7 @@
 	</div>
 	<div class="overflow-auto" style="max-height: 1000px;">
 		{#if zoom_list}
-			{#each zoom_list as zoom, i}
-				<div class="flex relative pb-2">
-					<div class="w-4 h-4 border border-green-500 rounded-full {!isPast(zoom) ? 'bg-white' : 'bg-green-500'}">
-						{#if i < zoom_list.length - 1}
-							<div class="absolute w-0.5 bg-green-500 inset-y-0 ml-1.5 top-1/2 h-full"></div>
-						{/if}
-					</div>
-					<div class="ml-4">
-						<p class="text-gray-500">
-							{dayjs(zoom.start_date).format('DD MMM, h:mma')}
-						</p>
-						<div>
-							{#if zoom.days.length}
-								{#each zoom.days as d}
-									<div class="cursor-pointer hover:text-blue-700 hover:bg-gray-200 flex items-center mb-1 group rounded">
-										<p class="leading-tight">{d.title}</p>
-										<div class="ml-2">
-											<RateLabel rate={d.t_difficulty_rate}/>
-										</div>
-									</div>
-								{/each}
-							{:else}
-								<p class="text-gray-300">- No material -</p>
-							{/if}
-						</div>
-					</div>
-				</div>
-			{/each}
+			<CourseTimeLine {zoom_list}/>
 		{/if}
 	</div>
 </div>
-
-<style>
-
-</style>
