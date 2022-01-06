e7f0f1225a4e367f65f7e450c5e85af4b1853026
cliff <cliff.wong@ehlacademy.org>
2022-01-06 15:56:08 +0800

    move message widget

--- src/routes/students/[student_id]/tutor-group/[tutor_group_id].svelte
+++ src/routes/students/[student_id]/tutor-group/[tutor_group_id].svelte
@@ -15,36 +15,41 @@
 	import dayjs from "dayjs";
 	import {tutor_group_store} from "../../../../store/index.js";
 	import RateLabel from '$lib/zoom/rate-label.svelte'
+	import MessageWidget from '$lib/message/message-widget.svelte'
 	$: tutor_group_id = $page.params.tutor_group_id
+	$: student_id = $page.params.student_id
 	$: tutor_group = $tutor_group_store.tutor_group[tutor_group_id]
 </script>
 
 <h1 class="font-bold">{tutor_group && tutor_group.title}</h1>
-<div class="p-4 bg-gray-200">
-	<div class="bg-white p-4">
-		{#each $course_store[$page.params.tutor_group_id] as zoom}
-			<div class="flex relative py-2">
-				<div class="w-6 h-6 rounded-full bg-green-500">
-					<div class="absolute w-0.5 bg-green-500 inset-y-0 ml-2.5 top-1/2 h-full"></div>
-				</div>
-				<div class="ml-4">
-					<p class="text-gray-500" class:opacity-50={dayjs(zoom.start_date).isBefore(dayjs())}>
-						{dayjs(zoom.start_date).format('DD MMM, h:mma')}
-					</p>
-					<div>
-						{#each zoom.days as d}
-							<div on:click={() => {previewMaterial(d)}} class="cursor-pointer hover:text-blue-700 hover:bg-gray-200 flex items-center mb-1 group py-0.5 rounded">
+<div class="flex">
+	<div class="p-4 bg-gray-200">
+		<div class="bg-white p-4">
+			{#each $course_store[$page.params.tutor_group_id] as zoom}
+				<div class="flex relative py-2">
+					<div class="w-6 h-6 rounded-full bg-green-500">
+						<div class="absolute w-0.5 bg-green-500 inset-y-0 ml-2.5 top-1/2 h-full"></div>
+					</div>
+					<div class="ml-4">
+						<p class="text-gray-500" class:opacity-50={dayjs(zoom.start_date).isBefore(dayjs())}>
+							{dayjs(zoom.start_date).format('DD MMM, h:mma')}
+						</p>
+						<div>
+							{#each zoom.days as d}
+								<div on:click={() => {previewMaterial(d)}} class="cursor-pointer hover:text-blue-700 hover:bg-gray-200 flex items-center mb-1 group py-0.5 rounded">
 
-								<p class="leading-tight">{d.title}</p>
-								<div class="w-14 flex-shrink-0 ml-auto">
-									<div class="bg-white px-2 rounded-full text-xs ml-auto hidden group-hover:block">Preview</div>
+									<p class="leading-tight">{d.title}</p>
+									<div class="w-14 flex-shrink-0 ml-auto">
+										<div class="bg-white px-2 rounded-full text-xs ml-auto hidden group-hover:block">Preview</div>
+									</div>
+									<RateLabel rate={d.t_difficulty_rate}/>
 								</div>
-								<RateLabel rate={d.t_difficulty_rate}/>
-							</div>
-						{/each}
+							{/each}
+						</div>
 					</div>
 				</div>
-			</div>
-		{/each}
+			{/each}
+		</div>
 	</div>
+	<MessageWidget {tutor_group_id} {student_id} height="calc(100vh - 170px)"/>
 </div>
