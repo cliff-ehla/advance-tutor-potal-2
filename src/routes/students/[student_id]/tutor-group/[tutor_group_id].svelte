6ef195339369d66a78399b8887df9d653e28b032
cliff <cliff.wong@ehlacademy.org>
2022-01-20 17:51:56 +0800

    fine tune

--- src/routes/students/[student_id]/tutor-group/[tutor_group_id].svelte
+++ src/routes/students/[student_id]/tutor-group/[tutor_group_id].svelte
@@ -15,15 +15,39 @@
 	import {tutor_group_store} from "../../../../store/index.js";
 	import MessageWidget from '$lib/message/message-widget.svelte'
 	import CourseTimeLine from '$lib/zoom/course-timeline.svelte'
+	import Icon from '$lib/ui/icon.svelte'
+	import dayjs from "dayjs";
 
 	$: tutor_group_id = $page.params.tutor_group_id
 	$: student_id = $page.params.student_id
 	$: tutor_group = $tutor_group_store.tutor_group[tutor_group_id]
 	$: zoom_list = $course_store[tutor_group_id]
+	$: course_start_date = zoom_list[0].start_date
+	$: course_end_date = zoom_list[zoom_list.length - 1].start_date
+	$: course_completed = dayjs().isAfter(dayjs(course_end_date))
 </script>
 
 <div class="p-4 pt-0">
-	<h1 style="font-size: 1.5em" class="font-light mb-4">Tutor group title [TODO]</h1>
+	<div class="mb-4">
+		<h1 style="font-size: 1.5em" class="font-light">Tutor group title [TODO]</h1>
+		<div class="flex items-center mt-1">
+			<Icon name="calendar" className="w-6 text-gray-400"/>
+			<p class="ml-2 text-gray-500">{dayjs(course_start_date).format('DD MMM')} - {dayjs(course_end_date).format('DD MMM')}</p>
+			<div class="ml-4">
+				{#if course_completed}
+					<div class="text-sm px-4 py-1 bg-blue-100 rounded-full inline-flex items-center">
+						<div class="w-2 h-2 rounded-full bg-gray-400"></div>
+						<p class="ml-2 text-gray-700">Course completed</p>
+					</div>
+				{:else}
+					<div class="text-sm px-4 py-1 bg-yellow-100 rounded-full inline-flex items-center">
+						<div class="w-2 h-2 rounded-full bg-yellow-500"></div>
+						<p class="ml-2 text-yellow-600">In progress</p>
+					</div>
+				{/if}
+			</div>
+		</div>
+	</div>
 	<div class="section-box">
 		{#if zoom_list}
 			<CourseTimeLine {zoom_list}/>
