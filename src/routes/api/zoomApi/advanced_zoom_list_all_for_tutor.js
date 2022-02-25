228b33a36d494f1d7f994774b3783832255c58f6
cliff <cliff.wong@ehlacademy.org>
2022-02-25 16:21:52 +0800

    fix api params wrog

--- src/routes/api/zoomApi/advanced_zoom_list_all_for_tutor.js
+++ src/routes/api/zoomApi/advanced_zoom_list_all_for_tutor.js
@@ -2,7 +2,7 @@ import {usermodel} from "$lib/usermodal";
 
 export const post = (req) => {
 	return usermodel(req, '/zoomApi/advanced_zoom_list_all_for_tutor', {
-		start_time: req.body.start_time,
-		end_time: req.body.end_time,
+		start_date: req.body.start_time,
+		end_date: req.body.end_time,
 	})
 }
