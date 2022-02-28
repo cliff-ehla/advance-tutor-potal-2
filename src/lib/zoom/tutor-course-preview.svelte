44d393a986462262bd30ae3c5b9421fea91315e1
cliff <cliff.wong@ehlacademy.org>
2022-02-28 17:33:54 +0800

    shortcut to check timeine

--- src/lib/zoom/tutor-course-preview.svelte
+++ src/lib/zoom/tutor-course-preview.svelte
@@ -1,10 +1,42 @@
 <script>
+	import {http} from "$lib/http.js";
+
 	export let course
 	import {rc_level_to_label} from "$lib/store/rc-level-to-label.js";
+	import TutorCourseTimeline from '$lib/zoom/tutor-course-timeline.svelte'
+	import Dropdown from '$lib/ui/dropdown3.svelte'
+	import Spinner from '$lib/ui/spinner.svelte'
+	import Icon from '$lib/ui/icon.svelte'
+	let existing_classroom
+
+	const onHover = async () => {
+		if (existing_classroom) return
+		const res = await http.post(fetch, '/tutorCourseApi/list_tutor_course_by_id', {
+			tutor_course_id: course.tutor_course_id
+		})
+		if (res.success) {
+			let data = res.data
+			existing_classroom = data.existing_classroom
+		}
+	}
 </script>
 
 <a href="/course/{course.tutor_course_id}" class="block p-4 border hover:shadow-lg transition-shadow rounded bg-white border-blue-300 border border-gray-200">
-	<p class="text-purple-800 text-sm leading-tight mt-1">{course.sub_cat}</p>
+	<div class="flex w-full mt-1">
+		<p class="flex-1 text-purple-800 text-sm leading-tight">{course.sub_cat}</p>
+		<Dropdown placement="right" activator_style="inline-flex items-center justify-center w-6 h-6 rounded-full border border-gray-300 ml-1 block">
+			<button slot="activator" on:mouseenter={onHover}>
+				<Icon className="w-3 text-gray-500" name="info"/>
+			</button>
+			<div class="dropdown">
+				{#if existing_classroom}
+					<TutorCourseTimeline {existing_classroom}/>
+				{:else}
+					<Spinner/>
+				{/if}
+			</div>
+		</Dropdown>
+	</div>
 	<p class="text-xs bg-purple-400 rounded-sm font-bold text-white px-2 py-0.5 leading-tight inline-block">
 		#{course.description_code_short_id}
 	</p>
