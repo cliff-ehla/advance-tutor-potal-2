e13243bfe2b4907290d1a4e2efcfd8bcb810c1ff
cliff <cliff.wong@ehlacademy.org>
2022-03-01 10:54:06 +0800

    handle no materail is uploaded

--- src/lib/pdf-reader/pdf-reader-dialog.svelte
+++ src/lib/pdf-reader/pdf-reader-dialog.svelte
@@ -18,13 +18,11 @@
 	let pdf_json
 	let ppt_link
 	let youtube_link_obj
-	let reminder_visible = false
 
 	$: is_expired = dayjs().isAfter(end_date)
 	$: lesson_now = dayjs().isAfter(start_date) && dayjs().isBefore(end_date)
 
 	onMount(async () => {
-		if (start_date) setTimeout(() => {reminder_visible = true}, 600)
 		if (item_id) {
 			let {data} = await http.post(fetch, '/itemApi/get_by_ids', {
 				ids: [item_id]
@@ -45,23 +43,14 @@
 		<p class="text-white opacity-80">Loading...</p>
 	</div>
 {:else}
-	{#if reminder_visible && !lesson_now}
-		<div transition:fly={{y: 50}} class="fixed z-50 top-4 left-1/2 w-72 -ml-36 pl-4 pr-2 py-2 rounded-lg bg-yellow-100 border border-yellow-500 shadow-lg text-center flex items-center">
-			<div class="text-yellow-800">
-				{#if is_expired}
-					Lesson expired <i class="underline">{dayjs(start_date).fromNow()}</i>
-				{:else}
-					Lesson will start <i class="underline">{dayjs(start_date).fromNow()}</i>
-				{/if}
-			</div>
-			<button on:click={() => {reminder_visible = false}} class="cc w-6 h-6 rounded-full ml-auto bg-yellow-200 text-yellow-800 hover:bg-yellow-300">
-				<Icon name="close" className="w-3"/>
-			</button>
-		</div>
-	{/if}
 	{#if pdf_json}
 		<Reader close_modal_button_visible {youtube_link_obj} pages_info_2={pdf_json}/>
 	{:else}
-		<button on:click={closeModal}>Close</button>
+		<div class="w-96 h-72 cc bg-white rounded text-center">
+			<div>
+				<p class="mb-4">Material not ready yet</p>
+				<button class="button" on:click={closeModal}>Close</button>
+			</div>
+		</div>
 	{/if}
 {/if}
